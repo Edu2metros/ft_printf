62a28835a83ed906313e54b1d75ef95d4983e193
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-08-29 20:44:09 +0000

    Aug 29, 2023, 5:44 PM

--- aa.c
+++ aa.c
@@ -1,1 +1,81 @@
-/* ### oiiiiiiiiiiiiiiiii */
+# include <stdlib.h>
+# include <unistd.h>
+
+static size_t	count_words(const char *s, char c);
+static char		*create_word(const char **s, char c);
+
+char	**ft_split(char const *string, char chr)
+{
+	size_t	words;
+	size_t	i;
+	char	**result;
+
+	words = count_words(string, chr);
+	result = (char **)malloc((words + 1) * sizeof(char *));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (*string && words > 0)
+	{
+		if (chr == *string)
+			string++;
+		else
+		{
+			result[i++] = create_word(&string, chr);
+			words--;
+		}
+	}
+	result[i] = NULL;
+	return (result);
+}
+
+static size_t	count_words(const char *s, char c)
+{
+	size_t	words_number;
+
+	words_number = 0;
+	while (*s)
+	{
+		if (*s == c)
+			s++;
+		else
+		{
+			words_number++;
+			while (*s && *s != c)
+				s++;
+		}
+	}
+	return (words_number);
+}
+
+static char	*create_word(const char **s, char c)
+{
+	const char	*start;
+	size_t		size;
+	char		*new_word;
+	const char	*src;
+	char		*dst;
+
+	start = *s;
+	while (**s && **s != c)
+		(*s)++;
+	size = *s - start;
+	new_word = (char *)malloc((size + 1) * sizeof(char));
+	if (!new_word)
+		return (NULL);
+	src = start;
+	dst = new_word;
+	while (src < *s)
+	{
+		*dst = *src;
+		dst++;
+		src++;
+	}
+	*dst = '\0';
+	return (new_word);
+}
+
+int main(void)
+{
+
+}

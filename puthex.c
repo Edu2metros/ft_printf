99f24c23061bdf30a51db42f197fd0b79664238c
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-08-30 22:40:32 +0000

    Aug 30, 2023, 7:40 PM

--- puthex.c
+++ puthex.c
@@ -6,7 +6,7 @@
 /*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 18:45:17 by eddos-sa          #+#    #+#             */
-/*   Updated: 2023/08/30 19:38:53 by eddos-sa         ###   ########.fr       */
+/*   Updated: 2023/08/30 19:39:58 by eddos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,6 @@ int	ft_puthex(unsigned int nbr, char x)
 	size = 0;
 	if (nbr / 16 > 0)
 		size += ft_puthex(nbr / 16, x);
-	else
-	{
-		size += ft_putnbr(nbr / 10);
-		size += ft_putchar(nbr % 10 + '0');
-	}
+
 	return (size);
 }

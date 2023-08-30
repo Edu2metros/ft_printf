26e6c2e89f0b4a040bbc7c48795d2bea3e3a6a54
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-08-30 16:24:29 +0000

    Aug 30, 2023, 1:24 PM

--- aa.c
+++ aa.c
@@ -6,7 +6,7 @@
 /*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 18:45:17 by eddos-sa          #+#    #+#             */
-/*   Updated: 2023/08/30 12:41:09 by eddos-sa         ###   ########.fr       */
+/*   Updated: 2023/08/30 13:23:51 by eddos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,5 @@
 
 int	main(void)
 {
-	int	decimalValue = 43;
-	printf("Valor em decimal: %d\n", decimalValue);
-	printf("Valor em hexadecimal (minúsculas): %x\n", decimalValue);
-	printf("Valor em hexadecimal (maiúsculas): %X\n", decimalValue);
+printf("%x\n", 120842);
 }

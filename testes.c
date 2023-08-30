91f860ab895d3a11526b07053467b850ac6b297e
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-08-30 21:56:46 +0000

    Aug 30, 2023, 6:56 PM

--- testes.c
+++ testes.c
@@ -3,3 +3,22 @@ int main(void)
 {
 	printf("%x", -255);
 }
+
+// base: 0123456789abcdef
+size_t	ft_putnbr_base(long int num, char *base)
+{
+	int		base_len;
+	size_t	size;
+
+	base_len = ft_strlen(base);
+	size = 0;
+	if (num < 0)
+	{
+		num = -num;
+		size += ft_putchar('-');
+	}
+	if (num >= base_len)
+		size += ft_putnbr_base(num / base_len, base);
+	size += ft_putchar(base[num % base_len]);
+	return (size);
+}

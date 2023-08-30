ddf6c3aaf656ec56527767b4bfb5eb38049d6dbe
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-08-30 20:36:46 +0000

    Aug 30, 2023, 5:36 PM

--- putint.c
+++ putint.c
@@ -6,41 +6,31 @@
 /*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 18:46:16 by eddos-sa          #+#    #+#             */
-/*   Updated: 2023/08/30 17:33:25 by eddos-sa         ###   ########.fr       */
+/*   Updated: 2023/08/30 17:36:46 by eddos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-int ft_putnbr(int nbr)
+int	ft_putnbr(int nbr)
 {
-    int size = 0;
+	int	size;
 
-    if (nbr < 0)
-    {
-        size += ft_putchar('-');
-        nbr = -nbr;
-    }
-
-    if (nbr < 10)
-    {
-        size += ft_putchar(nbr + '0');
-    }
-    else
-    {
-        size += ft_putnbr(nbr / 10);
-        size += ft_putchar(nbr % 10 + '0');
-    }
-
-    return size;
+	size = 0;
+	if (nbr < 0)
+	{
+		size += ft_putchar('-');
+		nbr = -nbr;
+	}
+	if (nbr < 10)
+	{
+		size += ft_putchar(nbr + '0');
+	}
+	else
+	{
+		size += ft_putnbr(nbr / 10);
+		size += ft_putchar(nbr % 10 + '0');
+	}
+	return (size);
 }
-
-int main(void)
-{
-    int size;
-
-    printf("\n%i\n", 92494754);
-    size = ft_putnbr(92494754);
-    printf("\n%i\n", size);
-}
-
+int ft_putnbr()

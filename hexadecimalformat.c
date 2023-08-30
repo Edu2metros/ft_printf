63a24a1d05c689d9714bcee4123052e89d2bf3b3
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-08-30 17:08:17 +0000

    Aug 30, 2023, 2:08 PM

--- hexadecimalformat.c
+++ hexadecimalformat.c
@@ -6,7 +6,7 @@
 /*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 18:45:17 by eddos-sa          #+#    #+#             */
-/*   Updated: 2023/08/30 14:02:26 by eddos-sa         ###   ########.fr       */
+/*   Updated: 2023/08/30 14:08:03 by eddos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ size_t ft_hexa(int number)
 	size = 0;
 	while(number / 16 > 10)
 	{
-		
+		temp = number;	
 	}
 	return(size);
 }

863e6e06c3dcc7806ee2d0e2f67f58a62ef0f9c9
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-08-30 16:49:24 +0000

    Aug 30, 2023, 1:49 PM

--- hexadecimalformat.c
+++ hexadecimalformat.c
@@ -1,2 +1,37 @@
-// pra transformar algo em hexadecimal, precisa dividir ele constantemente até o int dele ficar 0, e é da direita pra esquerda que vai colocando
-// 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hexadecimalformat.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/29 18:45:17 by eddos-sa          #+#    #+#             */
+/*   Updated: 2023/08/30 13:48:34 by eddos-sa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+• %p The void * pointer argument has to be printed in hexadecimal format.
+• %x Prints a number in hexadecimal (base 16) lowercase format.
+• %X Prints a number in hexadecimal (base 16) uppercase format. */
+#include "printf.h"
+#include <stdio.h>
+
+size_t	ft_hexa_min(int number)
+{
+	size_t	size;
+
+	size = 0;
+
+	return(size);
+}
+int	main(void)
+{
+	int number = 5;
+	int number2 = 15;
+	ft_hexa_min(number);
+	printf("%c\n");
+	ft_hexa_min(number2);
+	printf("%c\n");
+	printf("%i\n", ft_hexa_min(number));
+}

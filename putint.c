f6e992ed3b312df90e8a848d699186377baee2ab
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-08-30 18:59:27 +0000

    Aug 30, 2023, 3:59 PM

--- putint.c
+++ putint.c
@@ -6,32 +6,44 @@
 /*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 18:46:16 by eddos-sa          #+#    #+#             */
-/*   Updated: 2023/08/29 18:57:18 by eddos-sa         ###   ########.fr       */
+/*   Updated: 2023/08/30 15:58:38 by eddos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-void	ft_putnbr(int nbr)
+int	ft_putnbr(int nbr)
 {
+	int size;
+	size = 0;
 	if (nbr == -2147483648)
 	{
 		write(1, "-", 1);
 		ft_putnbr(2);
 		ft_putnbr(147483648);
+		size++;
 	}
 	else if (nbr >= 0 && nbr < 10)
 	{
 		ft_putchar(nbr + '0');
+		size++;
 	}
 	else if (nbr > 9)
 	{
 		ft_putnbr(nbr / 10);
 		ft_putchar(nbr % 10 + '0');
+		size++;
 	}
 	else
 	{
 		ft_putchar('-');
 		ft_putnbr(nbr * -1);
+		size++;
 	}
+	return(size);
+}
+#include <stdio.h>
+int main(void)
+{
+	printf("\n%i\n", ft_putnbr(34345));
 }

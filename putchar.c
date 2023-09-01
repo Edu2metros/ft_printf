52568a8a3f18e521f51ce74c45b851ad523e00a3
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-09-01 19:12:55 +0000

    Sep 1, 2023, 4:12 PM

--- putchar.c
+++ putchar.c
@@ -6,20 +6,15 @@
 /*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 18:46:01 by eddos-sa          #+#    #+#             */
-/*   Updated: 2023/09/01 14:34:41 by eddos-sa         ###   ########.fr       */
+/*   Updated: 2023/09/01 16:12:20 by eddos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-size_t	ft_putchar(char c)
+size_t ft_putchar(char c)
 {
-	size_t	size;
-
-	size = 0;
-	write(1, &c, 1);
-	size++;
-	return (size);
+    return write(1, &c, 1);
 }
 
 int	put_percent(char c)

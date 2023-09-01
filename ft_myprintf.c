28b2735497cc9ce7f14c68c51342bd14349e80d4
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-09-01 19:07:54 +0000

    Sep 1, 2023, 4:07 PM

--- ft_myprintf.c
+++ ft_myprintf.c
@@ -6,14 +6,25 @@
 /*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 15:37:06 by eddos-sa          #+#    #+#             */
-/*   Updated: 2023/09/01 15:38:42 by eddos-sa         ###   ########.fr       */
+/*   Updated: 2023/09/01 16:07:22 by eddos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
-int ft_printf(const char *str, ...)
+
+int	ft_printf(const char *str, ...)
 {
 	va_list(ap);
-	
+	size_t size;
+	size = 0;
+
 	va_start(ap, str);
+	if(!str)
+		return(-1);
+	return(size);
+}
+
+int main(void)
+{
+	printf("%i");
 }

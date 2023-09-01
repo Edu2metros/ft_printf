fd94ad1c76bc6e9f76d9f7ae3a0301dfe1d96a66
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-09-01 22:45:11 +0000

    Sep 1, 2023, 7:45 PM

--- putint.c
+++ putint.c
@@ -6,7 +6,7 @@
 /*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 18:46:16 by eddos-sa          #+#    #+#             */
-/*   Updated: 2023/09/01 19:40:49 by eddos-sa         ###   ########.fr       */
+/*   Updated: 2023/09/01 19:44:37 by eddos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,27 @@
 
 int	ft_putnbr(int nbr)
 {
-	int	size;
-
-	size = 0;
+	int size;
 	if (nbr == -2147483648)
 	{
 		write(1, "-", 1);
 		ft_putnbr(2);
 		ft_putnbr(147483648);
 	}
-	if (nbr < 10)
+	else if (nbr >= 0 && nbr < 10)
 	{
-		size += ft_putchar(nbr + '0');
+		ft_putchar(nbr + '0');
+	}
+	else if (nbr > 9)
+	{
+		ft_putnbr(nbr / 10);
+		ft_putchar(nbr % 10 + '0');
 	}
 	else
 	{
-		size += ft_putnbr(nbr / 10);
-		size += ft_putchar(nbr % 10 + '0');
+		ft_putchar('-');
+		ft_putnbr(nbr * -1);
 	}
-	return (size);
 }
+
+

cb92fd49bd58dd76da0ec9ee324c5021366e76ca
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-09-01 23:28:37 +0000

    Sep 1, 2023, 8:28 PM

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 18:45:39 by eddos-sa          #+#    #+#             */
-/*   Updated: 2023/09/01 20:05:12 by eddos-sa         ###   ########.fr       */
+/*   Updated: 2023/09/01 20:28:06 by eddos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@
 int		ft_printf(const char *str, ...);
 int		format(const char *str, va_list ap);
 size_t	ft_putchar(char c);
-int		put_percent(char c);
 int		ft_puthex(unsigned int nbr, char x);
 int		ft_putnbr(int nbr);
 int		ft_putptr(void *ptr);

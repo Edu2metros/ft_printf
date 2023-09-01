e52637ae6bcc186c60cb2b6723aa3ab241f53bb3
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-09-01 23:11:15 +0000

    Sep 1, 2023, 8:11 PM

--- putint.c
+++ putint.c
@@ -6,42 +6,32 @@
 /*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 18:46:16 by eddos-sa          #+#    #+#             */
-/*   Updated: 2023/09/01 19:45:42 by eddos-sa         ###   ########.fr       */
+/*   Updated: 2023/09/01 20:10:15 by eddos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-int ft_putnbr(int nbr)
+
+int	ft_putnbr(int nbr)
 {
-    int size = 0;
+	int	size;
 
-    if (nbr == -2147483648)
-    {
-        write(1, "-", 1);
-        size += 1;
-        size += ft_putnbr(2);
-        size += ft_putnbr(147483648);
-    }
-    else if (nbr >= 0 && nbr < 10)
-    {
-        ft_putchar(nbr + '0');
-        size += 1;
-    }
-    else if (nbr > 9)
-    {
-        size += ft_putnbr(nbr / 10);
-        ft_putchar(nbr % 10 + '0');
-        size += 1;
-    }
-    else
-    {
-        ft_putchar('-');
-        size += 1;
-        size += ft_putnbr(nbr * -1);
-    }
-
-    return size;
+	size = 0;
+	if (nbr == -2147483648)
+	{
+		write(1, "-", 1);
+		size++;
+		nbr = 147483648;
+	}
+	if (nbr < 0)
+	{
+		write(1, "-", 1);
+		nbr *= -1;
+		size++;
+	}
+	if (nbr >= 10)
+		size += ft_putnbr(nbr / 10);
+	ft_putchar(nbr % 10 + '0');
+	size++;
+	return (size);
 }
-
-
-

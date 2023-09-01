1283ccd49b3cfa86c1e35724616552292c0b6726
Eduardo Dos Santos Gomes <eddos-sa@c2r11p3.42sp.org.br>
2023-09-01 22:36:54 +0000

    Sep 1, 2023, 7:36 PM

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: eddos-sa <eddos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 15:37:06 by eddos-sa          #+#    #+#             */
-/*   Updated: 2023/09/01 19:32:32 by eddos-sa         ###   ########.fr       */
+/*   Updated: 2023/09/01 19:36:23 by eddos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,11 +63,11 @@ int	ft_printf(const char *str, ...)
 	return (sizetotal);
 }
 
-int	main(void)
+/* int	main(void)
 {
 	int size = ft_printf("  %x  ", 10);
 	printf("\n%i\n", size);
 	size = printf("  %x  ", 10);
 	printf("\n%i\n", size);
 
-}
+} */
